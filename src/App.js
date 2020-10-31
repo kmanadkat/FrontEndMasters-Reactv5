1a26fad040b69f36deef1fc796c0b7322e00b26c
Krupesh Anadkat <kmanadkat@gmail.com>
2020-10-31 15:58:45 +0530

    Configured ESLint for React & JSX

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Alex",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Alex" animal="Dog" breed="Havanese" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <Pet name="Stuart" animal="Mouse" breed="Lunar" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
