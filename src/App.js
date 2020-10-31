29ff8d60f9cfc1103377d2dbc195b5b34978b3b9
Krupesh Anadkat <kmanadkat@gmail.com>
2020-10-31 20:24:13 +0530

    Added REACH Router - Home & Details Page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
-// import Pet from "./Pet";
+import { Link, Router } from "@reach/router";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-      {/* <Pet name="Alex" animal="Dog" breed="Havanese" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
-      <Pet name="Stuart" animal="Mouse" breed="Lunar" /> */}
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 };
